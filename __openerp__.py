2946b2ea91d3a94a55e637f91c46685a1250ee9b
pankajJoshi <pankaj143342@gmail.com>
2014-04-15 11:53:11 +0530

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'GitHub Integration Module',
+    'version': '1.0',
+    'author': 'OpenERP DEx',
+    'depends': [
+        'base',
+    ],    
+    'data': [
+        'github_module_view.xml',
+    ],
+    'demo': [
+    ],
+    'css': [
+        'static/src/css/github_module_style.css'
+    ],
+    'js': [
+        'static/src/js/github_module_script.js'
+    ],
+    'qweb': [
+        'static/src/xml/git_module.xml'
+    ],
+    'active': False,
+    'installable': True,
+}
